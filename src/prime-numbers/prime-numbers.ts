2765ffd769bb2e34102eb97c777e189c99c29e64
Cole Groff <groffcole@gmail.com>
2020-12-23 22:27:18 -0600

    a little more efficient prime decision

--- src/prime-numbers/prime-numbers.ts
+++ src/prime-numbers/prime-numbers.ts
@@ -2,14 +2,23 @@ export const isPrimeNumber = (givenNumber: number): boolean => {
   // starting with 2 because 2 is the first prime number
   let divisor = 2;
 
-  while (givenNumber > divisor) {
-    // if the quotient has no remainder,
-    // then the given number is_not prime
+  // there is no need to check when the divisor is greater than half of the given number
+  // because any number will not be divisible by a number bigger than half of it
+  while (divisor <= givenNumber / 2) {
+    // if the quotient has no remainder then the given number is_not prime
     if (givenNumber % divisor === 0) {
       return false;
     }
-    divisor++;
+
+    if (divisor <= 3) {
+      divisor++;
+    } else {
+      // divisors greater than three can be increased by 2 because any number 
+      // divisible by an even number is divisible by 2
+      divisor += 2;
+    }
   }
 
+  // if we've made it this far then the given number is prime
   return true;
 };
--- test/prime-numbers/prime-numbers.test.ts
+++ test/prime-numbers/prime-numbers.test.ts
@@ -4,9 +4,11 @@ test.each([
   [12, false],
   [2, true],
   [11, true],
-  [997, true]
+  [997, true],
+  [137, true],
+  [237, false]
 ])(
-  "should determine if it is a prime number - %s",
+  "should determine if it is a prime number - %s, %s",
   (givenNumber: number, expectedResult: boolean) => {
     expect(isPrimeNumber(givenNumber)).toBe(expectedResult);
   }
